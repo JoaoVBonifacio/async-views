a64e011fc1f533d01fb2e353325bbee0fe6f18ed
JoaoVBonifácio <jboni2014@gmail.com>
2025-05-13 10:49:46 -0300

    Update views.py

--- asyncviews/views.py
+++ asyncviews/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 
 async def http_call_async():
-    for num in range(1, 20):
+    for num in range(1, 15):
         await asyncio.sleep(1)
         print(num)
     async with httpx.AsyncClient() as client:
